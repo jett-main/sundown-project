56b4afdcd6af1c4a5b646c73aeb172dfb0f75165
Shubham Chaubey <91285948+jett-main@users.noreply.github.com>
2024-07-17 14:11:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true
+});
+
+
+var elemC = document.querySelector("#elem-container");
+var fixed = document.querySelector("fixed-image");
+elemC.addEventListener("mouseenter", function(){
+      fixed.style.display = "block"
+})
+elemC.addEventListener("mouseleave", function(){
+      fixed.style.display = "none"
+})
